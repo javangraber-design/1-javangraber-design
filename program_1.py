d5e3e7ad2f9c37cee2527d3ca634f7c997a63ea2
javangraber-design <javangraber@gmail.com>
2026-01-22 16:06:37 -0600

    Javan Update program_1.py

--- program_1.py
+++ program_1.py
@@ -1,6 +1,9 @@
+#Programmer: Javan Graber
+#Date: January 22, 2026
 def hello_world():
     # Update the message below to say "Hello World", 
     # Python cares about white space, so watch your indentation.
-    print('Hello ...')
+    #Display text
+    print('Hello World')
 
 hello_world()
