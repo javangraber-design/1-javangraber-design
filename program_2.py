ae11978caabf5e9cdcac8c1d9e5ac743795d9ee5
javangraber-design <javangraber@gmail.com>
2026-01-22 16:15:22 -0600

    Javan Update program_2.py

--- program_2.py
+++ program_2.py
@@ -1,6 +1,10 @@
+#Programmer: Javan Graber
+#Date: January 22, 2026
 def proverbs_22_29():
     # Write your Proverbs 22:29 code below
-    print('...')
-    
-
+    #Display text
+    print('“Do you see a man skillful in his work?')
+    print('He will stand before kings;')
+    print('he will not stand before obscure men.”')
+    print('     (Proverbs 22:29)')
 proverbs_22_29()
